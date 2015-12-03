caaf843b39635b0d4ed64b35bfa76d9fee85f73d
minervaDutta <dutta.minerva@gmail.com>
2015-12-02 20:28:36 -0800

    modified comments

--- webserver-skeleton/webserver.js
+++ webserver-skeleton/webserver.js
@@ -1,11 +1,6 @@
 // Create a div at the end of the document with the id
 // "myfooter" and update it to demonstrate that javascript
-// processing works in the simple server for a standard browser
-// (i.e. show that the server correctly serves js files).
-
-// Note that this is raw javascript.
-// For a production site you would better off using libraries
-// like angularjs or jquery to hide browser quirks.
+// processing works 
 
 window.onload = function() {
     // Find the 'myfooter' element.
